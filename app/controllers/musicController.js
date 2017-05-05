73a3ba3c33e2ef93d954049987abc35b3392516b
Kate Natochii <my@example.com>
2017-05-06 01:28:45 +0300

    test

--- app/controllers/musicController.js
+++ app/controllers/musicController.js
@@ -3,6 +3,7 @@ var myApp = angular.module('mainModule');
 myApp.controller('musicController', ['$scope','vkMusicService', function($scope, vkMusicService) {
         $scope.test = 'Hola!';
         $scope.musicList = vkMusicService.getMusic();
+        $scope.masya = "1234567";
 }]);
 
 //todo angularjs dependency injection
